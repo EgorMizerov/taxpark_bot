2ed531b36afbbe12875952c08769de4517ab9e5d
Egor Mizerov <e.mizerov@alkor-tech.ru>
2024-04-24 22:25:38 +0500

    merge main

--- src/keyboard/user/keyboards.py
+++ src/keyboard/user/keyboards.py
@@ -47,12 +47,11 @@ def user_menu_markup() -> InlineKeyboardMarkup:
         callback_data=CallbackEnum.GO_TO_SUBSCRIPTION_SERVICES
     )
     first_row = [current_order_button]
-    second_row = [start_shift_button]
-    third_row = [user_statistics_button, profile_manager_button]
-    fourth_row = [promotions_statistics_button, info_and_help_button]
-    fifth_row = [faq_button]
-    sixth_row = [subscription_services_button]
-    lines = [first_row, second_row, third_row, fourth_row, fifth_row, sixth_row]
+    second_row = [start_shift_button, user_statistics_button]
+    third_row = [promotions_statistics_button, profile_manager_button]
+    fourth_row = [faq_button, info_and_help_button]
+    fifth_row = [subscription_services_button]
+    lines = [first_row, second_row, third_row, fourth_row, fifth_row]
     markup = InlineKeyboardMarkup(row_width=2, resize_keyboard=True, inline_keyboard=lines)
     return markup
 
